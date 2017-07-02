ff59dc22a4074d5b1932b09f0cecbdf0c695d674
andersonbaum <anderson.baum@gmail.com>
2017-07-01 23:45:23 -0300

    Incluindo formulário - incluindo teste

--- hello/test.py
+++ hello/test.py
@@ -1,53 +1,91 @@
 # -*- coding: utf-8 -*-
-from selenium import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
 
-class test(unittest.TestCase):
+class Test2(unittest.TestCase):
     def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "file:///C:/Users/Anderson/Google%20Drive/Estudo/Programa%C3%A7%C3%A3o/Python/trabanderson/hello/templates/index.html"
         self.verificationErrors = []
-        self.selenium = selenium("localhost", 4444, "*firefox", "https://trabanderson.herokuapp.com/")
-        self.selenium.start()
+        self.accept_next_alert = True
 
-    def test_test(self):
-        sel = self.selenium
-        sel.open("/")
-        sel.type("name=nome", "Anderson")
-        sel.type("name=sobrenome", "Barbosa")
-        sel.type("name=dia", "26")
-        sel.type("name=mes", "08")
-        sel.type("name=ano", "1985")
-        sel.type("name=rg", "8087")
-        sel.type("name=cpf", "000")
-        sel.type("name=cpf2", "00")
-        sel.type("name=rua", "Rua")
-        sel.type("name=numero", "123")
-        sel.type("name=bairro", "Nonoai")
-        sel.select("name=estado", "label=Rio de Janeiro")
-        sel.select("name=estado", "label=Rio Grande do Norte")
-        sel.select("name=estado", u"label=Rondônia")
-        sel.select("name=estado", "label=Rio Grande do Norte")
-        sel.select("name=estado", u"label=Rondônia")
-        sel.select("name=estado", "label=Rio Grande do Norte")
-        sel.select("name=estado", "label=Rio de Janeiro")
-        sel.select("name=estado", u"label=Piauí")
-        sel.select("name=estado", "label=Rio de Janeiro")
-        sel.select("name=estado", "label=Rio Grande do Norte")
-        sel.select("name=estado", u"label=Rondônia")
-        sel.select("name=estado", "label=Rio Grande do Sul")
-        sel.select("name=estado", "label=Roraima")
-        sel.select("name=estado", "label=Rio Grande do Sul")
-        sel.type("name=cidade", "Porto Alegre")
-        sel.type("name=cep", "9172")
-        sel.type("name=cep2", "0")
-        sel.type("name=email", "anderson.baum@gmail.com")
-        sel.type("name=login", "aaa")
-        sel.type("name=pass", "aaa")
-        sel.type("name=passconfirm", "aaa")
-        sel.click("//input[@value='Limpar']")
+    def test_2(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_name("nome").clear()
+        driver.find_element_by_name("nome").send_keys("Anderson")
+        driver.find_element_by_name("sobrenome").clear()
+        driver.find_element_by_name("sobrenome").send_keys("Barbosa")
+        driver.find_element_by_name("dia").clear()
+        driver.find_element_by_name("dia").send_keys("26")
+        driver.find_element_by_name("mes").clear()
+        driver.find_element_by_name("mes").send_keys("08")
+        driver.find_element_by_name("ano").clear()
+        driver.find_element_by_name("ano").send_keys("1985")
+        driver.find_element_by_name("rg").clear()
+        driver.find_element_by_name("rg").send_keys("8087")
+        driver.find_element_by_name("cpf").clear()
+        driver.find_element_by_name("cpf").send_keys("000")
+        driver.find_element_by_name("cpf2").clear()
+        driver.find_element_by_name("cpf2").send_keys("00")
+        driver.find_element_by_name("rua").clear()
+        driver.find_element_by_name("rua").send_keys("Rua")
+        driver.find_element_by_name("numero").clear()
+        driver.find_element_by_name("numero").send_keys("123")
+        driver.find_element_by_name("bairro").clear()
+        driver.find_element_by_name("bairro").send_keys("Nonoai")
+        Select(driver.find_element_by_name("estado")).select_by_visible_text("Rio Grande do Sul")
+        driver.find_element_by_name("cidade").clear()
+        driver.find_element_by_name("cidade").send_keys("Porto Alegre")
+        driver.find_element_by_name("cep").clear()
+        driver.find_element_by_name("cep").send_keys("9172")
+        driver.find_element_by_name("cep2").clear()
+        driver.find_element_by_name("cep2").send_keys("0")
+        driver.find_element_by_name("email").clear()
+        driver.find_element_by_name("email").send_keys("anderson.baum@gmail.com")
+        driver.find_element_by_name("login").clear()
+        driver.find_element_by_name("login").send_keys("aaa")
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys("aaa")
+        driver.find_element_by_name("passconfirm").clear()
+        driver.find_element_by_name("passconfirm").send_keys("aaa")
+        driver.find_element_by_xpath("//input[@value='Limpar']").click()
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
 
     def tearDown(self):
-        self.selenium.stop()
+        self.driver.quit()
         self.assertEqual([], self.verificationErrors)
 
 
