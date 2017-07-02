eae226179bdaea42950ff1dbf8ea3715a21ea5fc
andersonbaum <anderson.baum@gmail.com>
2017-07-01 21:55:51 -0300

    Incluindo formulário - ajustes layout

---
+++ hello/test.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from selenium import selenium
+import unittest, time, re
+
+
+class test(unittest.TestCase):
+    def setUp(self):
+        self.verificationErrors = []
+        self.selenium = selenium("localhost", 4444, "*chrome", "https://trabanderson.herokuapp.com/")
+        self.selenium.start()
+
+    def test_test(self):
+        sel = self.selenium
+        sel.open("/")
+        sel.type("name=nome", "Anderson")
+        sel.type("name=sobrenome", "Barbosa")
+        sel.type("name=dia", "26")
+        sel.type("name=mes", "08")
+        sel.type("name=ano", "1985")
+        sel.type("name=rg", "8087")
+        sel.type("name=cpf", "000")
+        sel.type("name=cpf2", "00")
+        sel.type("name=rua", "Rua")
+        sel.type("name=numero", "123")
+        sel.type("name=bairro", "Nonoai")
+        sel.select("name=estado", "label=Rio Grande do Sul")
+        sel.type("name=cidade", "Porto Alegre")
+        sel.type("name=cep", "9172")
+        sel.type("name=cep2", "0")
+        sel.type("name=email", "anderson.baum@gmail.com")
+        sel.type("name=login", "aaa")
+        sel.type("name=pass", "aaa")
+        sel.type("name=passconfirm", "aaa")
+        sel.click("//input[@value='Limpar']")
+
+    def tearDown(self):
+        self.selenium.stop()
+        self.assertEqual([], self.verificationErrors)
+
+
+if __name__ == "__main__":
+    unittest.main()
